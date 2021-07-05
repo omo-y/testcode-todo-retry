44ec0e0258fe9e2f7ddaab6895c5e58ead0c50c0
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-06 08:44:55 +0900

    ver3

--- src/index.js
+++ src/index.js
@@ -14,13 +14,22 @@ const onClickAdd = () => {
   //完了ボタン生成
   const compbutton = document.createElement("button");
   compbutton.innerText = "完了";
+  compbutton.addEventListener("click", () => {
+    alert("完了");
+  });
   li.appendChild(compbutton);
   //削除ボタン生成
   const delbutton = document.createElement("button");
   delbutton.innerText = "削除";
+  delbutton.addEventListener("click", () => {
+    //削除ボタンの親タグを未完了のTODOから削除
+    const delettarget = delbutton.parentNode;
+    console.log(delettarget);
+    document.getElementById("imcomplete-area").removeChild(delettarget);
+  });
   li.appendChild(delbutton);
-  //未完了リストに追加
-  console.log(div);
+  //未完了のtodoリストに追加
+  //console.log(div);
   document.getElementById("inconplete-list").appendChild(div);
 };
 
