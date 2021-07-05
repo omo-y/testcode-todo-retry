87d44b66a0f95fa6b309a954b0b41f2991fa89f3
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-06 05:04:51 +0900

    ver2 ニュウリョク~未完了TODO

--- src/index.js
+++ src/index.js
@@ -2,8 +2,26 @@ import "./styles.css";
 
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
-  alert(inputText);
   document.getElementById("add-text").value = "";
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //liタグ
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  //divタグの下に各子要素を設定
+  div.appendChild(li);
+  //完了ボタン生成
+  const compbutton = document.createElement("button");
+  compbutton.innerText = "完了";
+  li.appendChild(compbutton);
+  //削除ボタン生成
+  const delbutton = document.createElement("button");
+  delbutton.innerText = "削除";
+  li.appendChild(delbutton);
+  //未完了リストに追加
+  console.log(div);
+  document.getElementById("inconplete-list").appendChild(div);
 };
 
 document
